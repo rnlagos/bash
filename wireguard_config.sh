d4d2f23d727b3e86e01cdfebd136294fd068f91e
Rafael <rafa@tasarte.app>
2022-03-14 11:58:34 +0100

    Update wireguard_config.sh

--- wireguard_config.sh
+++ wireguard_config.sh
@@ -5,7 +5,5 @@ OLD=$(head -13 /etc/wireguard/wg0.conf | tail +13)
 OLDD=$(head -18 /etc/wireguard/wg0.conf | tail +18)
 
 if echo "$IP"; then
-
-sed -i "13 s/$OLD/Endpoint = $IP:51820/" /etc/wireguard/wg0.conf; sed -i "18 s/$OLDD/Endpoint = $IP:51820/" /etc/wireguard/wg0.conf
-
+  sed -i "13 s/$OLD/Endpoint = $IP:51820/" /etc/wireguard/wg0.conf; sed -i "18 s/$OLDD/Endpoint = $IP:51820/" /etc/wireguard/wg0.conf
 fi
