a9a453cb3a8fc75c98ccfc16f06478b1bfd0bd1d
Rafael <rafa@tasarte.app>
2022-11-28 17:40:59 +0100

    Create make_playlist_to_collection.sh

---
+++ make_playlist_to_collection.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export FOLDER=folder
+export NOMBRE_DIR=nombre_dir
+read -p "Carpeta: " FOLDER
+if [ "$FOLDER" != "" ]
+then 
+	cd "$FOLDER" && NOMBRE_DIR=$(pwd | sed 's,^\(.*/\)\?\([^/]*\),\2,') 
+	ls -1v | grep .mp3 > "$NOMBRE_DIR.m3u"
+	RUTA_M3U=$(python3 ~/Escritorio/plex/ruta.py)
+	python3 ~/Escritorio/plex/m3u_to_playlist.py -l 'Música' -f "$RUTA_M3U"
+	python3 ~/Escritorio/plex/playlist_to_collection.py -l 'Música' -p "$NOMBRE_DIR"
+fi
